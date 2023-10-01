30f78923034f525a75db53a16997c540fabcac83
Rasulyo <kkg.rasul@gmail.com>
2023-10-01 22:43:39 +0600

    imports

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
+import { createVfm } from 'vue-final-modal'
+import Notifications from '@kyvg/vue3-notification'
+import 'vue-final-modal/style.css'
 import './style.css';
 import App from './App.vue';
 import router from './routes';
 import store from './store/store';
-import { createVfm } from 'vue-final-modal'
-import 'vue-final-modal/style.css'
-import Notifications from '@kyvg/vue3-notification'
 
 createApp(App).use(router).use(store).use(createVfm()).use(Notifications).mount('#app');
